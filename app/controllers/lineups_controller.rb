180ccc5342cc792dec16716e1a5ab57940a290f3
OmarMontemayor <omarmontemayor11@yahoo.com>
2021-05-19 04:55:56 -0500

    finished lineup views

--- app/controllers/lineups_controller.rb
+++ app/controllers/lineups_controller.rb
@@ -1,5 +1,9 @@
 class LineupsController < ApplicationController
 
+    def index
+        @lineups = Lineup.all
+    end
+
     def new
         @lineup = Lineup.new
         @lineup.players.build
@@ -7,7 +11,7 @@ class LineupsController < ApplicationController
 
     def create
         @lineup = Lineup.new(lineup_params)
-
+        #binding.pry
         if @lineup.save
             redirect_to lineup_path(@lineup)
         else
@@ -17,13 +21,32 @@ class LineupsController < ApplicationController
 
     def show
         @lineup = Lineup.find(params[:id])
-        binding.pry
+        #binding.pry
+    end
+
+    def edit
+        @lineup = Lineup.find(params[:id])
+    end
+
+    def update
+        @lineup = Lineup.find(params[:id])
+        if @lineup.update(lineup_params)
+            redirect_to lineup_path(@lineup)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @lineup = Lineup.find(params[:id])
+        @lineup.destroy
+        redirect_to lineups_path
     end
 
     private
 
     def lineup_params
-        params.require(:lineup).permit(:name, player_attributes: [:id, :name, :position])
+        params.require(:lineup).permit(:name, players_attributes: [:id, :name, :position])
     end
 end
 
